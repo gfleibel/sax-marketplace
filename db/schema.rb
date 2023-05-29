9c132e4952c116cf7e6c4896033b334e2e376926
Vitoria Vital <vitoria.vital123@gmail.com>
2023-05-29 15:53:48 -0300

    add status column sax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_172910) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_185032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_172910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "price"
+    t.boolean "status"
     t.index ["user_id"], name: "index_saxophones_on_user_id"
   end
 
